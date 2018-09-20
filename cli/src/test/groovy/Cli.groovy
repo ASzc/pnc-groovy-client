952caf1fc865007aad5c3b37dc7b82185d0beeb6
Alex Szczuczko <aszczucz@redhat.com>
2018-09-20 11:51:29 -0600

    Add cli help tests

--- cli/src/test/groovy/Cli.groovy
+++ cli/src/test/groovy/Cli.groovy
@@ -6,8 +6,16 @@ class CliSpec extends Specification {
 
     def cli(String... args) {
         def o = new StringWriter()
-        def code = PncCli.cli(['-h'], new PrintWriter(o))
+        def code = PncCli.cli(args, new PrintWriter(o))
         return [code, o.toString()]
+    }
+
+    def "root returns help"() {
+        when:
+            def (code, out) = cli()
+        then:
+            code == 2
+            out.startsWith('error: Unknown subcommand null\nUsage: pgc [-d] {login,call,list}')
     }
 
     def "root help returns help"() {
@@ -16,5 +24,37 @@ class CliSpec extends Specification {
         then:
             code == 1
             out.startsWith('Usage: pgc [-d] {login,call,list}')
+    }
+
+    def "invalid subcommand returns help"() {
+        when:
+            def (code, out) = cli('asd', '-h')
+        then:
+            code == 2
+            out.startsWith('error: Unknown subcommand asd\nUsage: pgc [-d] {login,call,list}')
+    }
+
+    def "login help returns help"() {
+        when:
+            def (code, out) = cli('login', '-h')
+        then:
+            code == 3
+            out.startsWith('Usage: pgc login [-c]')
+    }
+
+    def "call help returns help"() {
+        when:
+            def (code, out) = cli('call', '-h')
+        then:
+            code == 3
+            out.startsWith('Usage: pgc call [-a ARGUMENT=VALUE] group operation')
+    }
+
+    def "list help returns help"() {
+        when:
+            def (code, out) = cli('list', '-h')
+        then:
+            code == 3
+            out.startsWith('Usage: pgc list [-e PATTERN]')
     }
 }
